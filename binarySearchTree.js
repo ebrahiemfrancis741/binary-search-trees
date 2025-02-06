35e4b7b180bebc8d57bf42af33c375dffbed6d86
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-02-06 06:00:59 +0200

    Add Node class

---
+++ binarySearchTree.js
@@ -0,0 +1,36 @@
+
+class Node {
+  #data;
+  #left;
+  #right;
+
+  get data() {
+    return this.#data;
+  }
+
+  set data(data) {
+    this.#data = data;
+  }
+
+  get left() {
+    return this.#left;
+  }
+
+  set left(left) {
+    this.#left = left;
+  }
+
+  get right() {
+    return this.#right;
+  }
+
+  set right(right) {
+    this.#right = right;
+  }
+
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
